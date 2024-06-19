dc5e316a56336caa67ef205f7266e6d61f5fe681
unknown <devmokhtar@gmail.com>
2024-06-19 19:49:20 +0300

    Update README file

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit7002244760848f9ad9746f4c64cdccba
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit7002244760848f9ad9746f4c64cdccba', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit7002244760848f9ad9746f4c64cdccba', 'loadClassLoader'));
@@ -31,6 +33,25 @@ class ComposerAutoloaderInit7002244760848f9ad9746f4c64cdccba
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit7002244760848f9ad9746f4c64cdccba::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire7002244760848f9ad9746f4c64cdccba($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire7002244760848f9ad9746f4c64cdccba($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
