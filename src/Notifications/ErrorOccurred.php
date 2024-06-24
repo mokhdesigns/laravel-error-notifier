d4d10a3c48746f0f806c5bf4a2321b800ea7b79a
unknown <devmokhtar@gmail.com>
2024-06-24 23:15:27 +0300

    Update package version to 1.0.4

--- src/Notifications/ErrorOccurred.php
+++ src/Notifications/ErrorOccurred.php
@@ -24,14 +24,55 @@ class ErrorOccurred extends Notification
 
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-            ->subject('Error Occurred in ' . config('app.name'))
-            ->line('An error has occurred:')
-            ->line($this->exception->getMessage())
-            ->line('File: ' . $this->exception->getFile())
-            ->line('Line: ' . $this->exception->getLine())
-            ->line('Stack Trace:')
-            ->line($this->exception->getTraceAsString())
-            ->line('Please address this issue promptly.');
+        // return (new MailMessage)
+        //     ->subject('Error Occurred in ' . config('app.name'))
+        //     ->line('An error has occurred:')
+        //     ->line($this->exception->getMessage())
+        //     ->line('File: ' . $this->exception->getFile())
+        //     ->line('Line: ' . $this->exception->getLine())
+        //     ->line('Stack Trace:')
+        //     ->line($this->exception->getTraceAsString())
+        //     ->line('Please address this issue promptly.');
+
+
+        $guards = array_keys(config('auth.guards'));
+
+        $user = null;
+
+        foreach ($guards as $guard) {
+            if (auth()->guard($guard)->check()) {
+                $user = auth()->guard($guard)->user();
+                break;
+            }
+        }
+
+            $routeName = request()->route() ? request()->route()->getName() : 'N/A';
+            $apiEndpoint = request()->fullUrl();
+            $requestMethod = request()->method();
+            $userAgent = request()->header('User-Agent');
+            $ipAddress = request()->ip();
+            $currentTime = now()->toDateTimeString();
+
+            $mailMessage = (new MailMessage)
+                ->subject('Error Occurred in ' . config('app.name'))
+                ->line('An error has occurred:')
+                ->line($this->exception->getMessage())
+                ->line('File: ' . $this->exception->getFile())
+                ->line('Line: ' . $this->exception->getLine())
+                // ->line('Stack Trace:')
+                // ->line($this->exception->getTraceAsString())
+                ->line('Route Name: ' . $routeName)
+                ->line('Endpoint: ' . $apiEndpoint)
+                ->line('Request Method: ' . $requestMethod)
+                ->line('User Agent: ' . $userAgent)
+                ->line('IP Address: ' . $ipAddress)
+                ->line('Occurred At: ' . $currentTime);
+
+            if ($user) {
+                $mailMessage->line('User: ' . $user->name . ' (' . $user->email . ')' . ' (' . $user->id . ')' . ' (' . $user->phone . ')');
+            }
+
+            return $mailMessage->line('Please address this issue promptly.');
+
     }
 }
